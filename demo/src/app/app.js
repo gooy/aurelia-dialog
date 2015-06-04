8436a9a3a584fdbea77ff1e4f6c13de06dd5efaf
Aaike Van Roekeghem <aaikevr@gmail.com>
2015-06-04 21:01:59 +0700

    update

--- demo/src/app/app.js
+++ demo/src/app/app.js
@@ -4,7 +4,7 @@ export class App {
 
   configureRouter(config, router){
 
-    config.title = 'Aurelia Dialog';
+    config.title = 'aurelia-dialog';
     config.map([
       { route: ['','home'],  moduleId: './home',      nav: true, title:'Home' },
       { route: 'demo',  moduleId: './demo',      nav: true, title:'Demo' },
